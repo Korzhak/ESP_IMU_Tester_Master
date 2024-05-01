c5990dcf4b5671aaff745e60a0bb6b3d52b7889b
Богдан Бельський <belskiy-bo@dvluch.loc>
2024-05-01 16:43:57 +0300

    fixed

--- src/main.cpp
+++ src/main.cpp
@@ -28,13 +28,15 @@ uint8_t broadcastAddress[] = {0x58, 0xBF, 0x25, 0x82, 0x6B, 0xF4};
 Adafruit_MPU6050 mpu;
 sensors_event_t a, g, temp;
 
+uint32_t lastSendTime = 0;
+
 /****************** ADDITIONAL FUNCTIONS ******************/
 // callback when data is sent
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
-{
-	Serial.print("\r\nLast Packet Send Status:\t");
-	Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-}
+// void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+// {
+// 	Serial.print("\r\nLast Packet Send Status:\t");
+// 	Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+// }
 
 /****************** MAIN FUNCTIONS ******************/
 void setup()
@@ -54,7 +56,7 @@ void setup()
 
 	// Once ESPNow is successfully Init, we will register for Send CB to
 	// get the status of Trasnmitted packet
-	esp_now_register_send_cb(OnDataSent);
+	// esp_now_register_send_cb(OnDataSent);
 
 	// Register peer
 	memcpy(peerInfo.peer_addr, broadcastAddress, 6);
@@ -79,33 +81,26 @@ void setup()
 	}
 
 	mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
-	mpu.setGyroRange(MPU6050_RANGE_250_DEG);
-	mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+	mpu.setGyroRange(MPU6050_RANGE_2000_DEG);
+	mpu.setFilterBandwidth(MPU6050_BAND_260_HZ);
 	Serial.println("");
 	delay(100);
 }
 
 void loop()
 {
-	mpu.getEvent(&a, &g, &temp);
+	if((millis() - lastSendTime) >= 10) {
+		mpu.getEvent(&a, &g, &temp);
 
-	imu.accX = a.acceleration.x;
-	imu.accY = a.acceleration.y;
-	imu.accZ = a.acceleration.z;
-	imu.gyrX = g.gyro.x;
-	imu.gyrY = g.gyro.y;
-	imu.gyrZ = g.gyro.z;
+		imu.accX = a.acceleration.x;
+		imu.accY = a.acceleration.y;
+		imu.accZ = a.acceleration.z;
+		imu.gyrX = g.gyro.x;
+		imu.gyrY = g.gyro.y;
+		imu.gyrZ = g.gyro.z;
 
-	// Send message via ESP-NOW
-	esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&imu, sizeof(imu));
-
-	if (result == ESP_OK)
-	{
-		Serial.println("Sent with success");
+		// Send message via ESP-NOW
+		esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&imu, sizeof(imu));
+		lastSendTime = millis();
 	}
-	else
-	{
-		Serial.println("Error sending the data");
-	}
-	delay(20);
 }
